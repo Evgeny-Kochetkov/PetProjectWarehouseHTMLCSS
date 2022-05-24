2b5cf3b5da8d4eb70b0a19e76b0fc399e0297ac4
Evgeny Kochetkov <jackeee1993@gmail.com>
2022-05-24 14:32:12 +0300

    add script and slider consts

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,9 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    
+    //Testimonials slider
 
-
-
-
+    const slidesWrapper = document.querySelector('.testimonials__grid');
+    const slides = document.querySelectorAll('.grid__user');
+    const arrowLeft = document.querySelector('.arrows__btn_left');
+    const arrowRight = document.querySelector('.arrows__btn_right');
+    const dots = document.querySelectorAll('.btn__dot');
+    const widthSliderWrapper = window.getComputedStyle(slidesWrapper).width;
+    let slideIndex = 1;
+    let offset = window.getComputedStyle(slidesWrapper).width;
 
 });
